df91e605ad0a10ed5d54949068169666860d4832
Viktor Benei <viktor.benei@gmail.com>
2016-11-15 19:56:33 +0100

    removed sleep/delay workaround

--- ios-xcode-8.0/ios-xcode-8.0UITests/ios_xcode_8_0UITests.swift
+++ ios-xcode-8.0/ios-xcode-8.0UITests/ios_xcode_8_0UITests.swift
@@ -18,10 +18,7 @@ class ios_xcode_8_0UITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        Thread.sleep(forTimeInterval: 10)
         XCUIApplication().launch()
-        Thread.sleep(forTimeInterval: 10)
-        print("Starting the UI tests!")
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
